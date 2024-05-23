f361047ffbb798090054d7f5cb4e61459ca722b5
Alice <30956835+alicefromkr@users.noreply.github.com>
2024-05-24 05:24:51 +0900

    Update Footer.tsx
    팀원들 이름 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,10 @@ const Footer = () => {
     <div className="container flex flex-row">
       <footer className="w-full bg-gray-800 text-white text-center py-8 bottom-0 absolute">
         <p className="text-sm sm:text-base">
-          ©{currentYear} AWS Cloud Schol 4기 CI/CD팀 3조!. All rights reserved.
+          ©{currentYear} AWS Cloud Schol 4기 CI/CD팀 3조. All rights reserved.
+        </p>
+        <p>
+          정만성, 김영우, 한규민, 박재연
         </p>
       </footer>
     </div>
